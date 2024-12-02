d729f725e0e1fe4e22e41204fc69f6db76797957
roland-pixel <kharis7181@gmail.com>
2024-12-02 15:23:37 +0700

    Generic Function

---
+++ bin/genericfunction.dart
@@ -0,0 +1,9 @@
+import 'helper/array_helper.dart';
+
+void main() {
+  var number = [1, 2, 3, 4, 5, 6];
+  var names = ["kharis", "raihan"];
+
+  print(ArrayHelper.count(number));
+  print(ArrayHelper.count(names));
+}
